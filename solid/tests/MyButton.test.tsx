9bdc5bda4f66ae497cf2975e7d696666a55f7352
Kevin Deng <sxzz@sxzz.moe>
2025-12-06 09:15:09 +0800

    refactor(solid): switch back to `@solidjs/testing-library`

--- solid/tests/MyButton.test.tsx
+++ solid/tests/MyButton.test.tsx
@@ -1,12 +1,10 @@
-import { render } from 'solid-testing-library'
+import { render } from '@solidjs/testing-library'
 import { expect, test } from 'vitest'
 import { MyButton } from '../src'
 
 test('button', () => {
   const screen = render(() => <MyButton type="primary" />)
-  const buttonElement = screen.getByText(/my button/i)
-
-  expect(buttonElement.outerHTML).toMatchInlineSnapshot(
+  expect(screen.container.innerHTML).toMatchInlineSnapshot(
     `"<button class="my-button">my button<br> type: primary<br> count: 0</button>"`,
   )
 })
--- solid/vite.config.ts
+++ solid/vite.config.ts
@@ -5,13 +5,10 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: './playground',
-  define: {
-    // solid-testing-library relies on "process" which is not shimmed by default
-    'process.env.STL_SKIP_AUTO_CLEANUP': 'false',
-  },
   plugins: [solid()],
   test: {
     root: '.',
+    environment: 'node',
     browser: {
       enabled: true,
       provider: playwright(),
