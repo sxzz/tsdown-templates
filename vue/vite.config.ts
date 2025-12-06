f30b647c9d79e8b166b2367d3c3514a27a13585a
Kevin Deng <sxzz@sxzz.moe>
2025-12-06 08:56:27 +0800

    refactor(vue): merge vitest config to vite

--- vue/vite.config.ts
+++ vue/vite.config.ts
@@ -1,7 +1,18 @@
+/// <reference types="vitest/config" />
+import { playwright } from '@vitest/browser-playwright'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: './playground',
   plugins: [vue()],
+  test: {
+    root: '.',
+    browser: {
+      enabled: true,
+      provider: playwright(),
+      instances: [{ browser: 'chromium' }],
+      headless: true,
+    },
+  },
 })
