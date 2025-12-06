654af22efa3b113826003638030b436dd078f2d1
Kevin Deng <sxzz@sxzz.moe>
2025-12-06 08:55:10 +0800

    refactor(react): switch to vitest browser mode

--- react-compiler/tests/index.test.tsx
+++ react-compiler/tests/index.test.tsx
@@ -1,19 +1,15 @@
-/// <reference types="@testing-library/jest-dom" />
-
-import { render, screen } from '@testing-library/react'
+import { render } from 'vitest-browser-react'
 import { expect, test } from 'vitest'
 import { MyButton } from '../src'
 
-test('button', () => {
-  render(<MyButton type="primary" />)
-
-  const buttonElement = screen.getByText(/my button/i)
+test('button', async () => {
+  const screen = await render(<MyButton type="primary" />)
+  const buttonElement = screen.getByRole('button')
 
   expect(buttonElement).toBeInTheDocument()
   expect(buttonElement).toHaveTextContent('my button type: primary count: 0')
-  expect(buttonElement.outerHTML).toMatchInlineSnapshot(
+  expect(screen.container.innerHTML).toMatchInlineSnapshot(
     `"<button class="my-button">my button<br> type: primary<br> count: 0</button>"`,
   )
-
   expect(buttonElement).toHaveClass('my-button')
 })
--- react-compiler/vite.config.ts
+++ react-compiler/vite.config.ts
@@ -1,7 +1,18 @@
+/// <reference types="vitest/config" />
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { playwright } from '@vitest/browser-playwright'
 
 export default defineConfig({
   root: './playground',
   plugins: [react({ babel: { plugins: ['babel-plugin-react-compiler'] } })],
+  test: {
+    root: '.',
+    browser: {
+      enabled: true,
+      provider: playwright(),
+      instances: [{ browser: 'chromium' }],
+      headless: true,
+    },
+  },
 })
