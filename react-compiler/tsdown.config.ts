5f22c62c73b166d2e586590fdde47c42601f5179
Kevin Deng <sxzz@sxzz.moe>
2026-03-15 18:57:34 +0800

    feat(react-compiler): use rolldown babel plugin

--- react-compiler/tsdown.config.ts
+++ react-compiler/tsdown.config.ts
@@ -1,17 +1,12 @@
-import pluginBabel from '@rollup/plugin-babel'
+import pluginBabel from '@rolldown/plugin-babel'
+import { reactCompilerPreset } from '@vitejs/plugin-react'
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   platform: 'neutral',
   plugins: [
     pluginBabel({
-      babelHelpers: 'bundled',
-      parserOpts: {
-        sourceType: 'module',
-        plugins: ['jsx', 'typescript'],
-      },
-      plugins: ['babel-plugin-react-compiler'],
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      presets: [reactCompilerPreset()],
     }),
   ],
 })
--- react-compiler/vite.config.ts
+++ react-compiler/vite.config.ts
@@ -1,13 +1,15 @@
 /// <reference types="vitest/config" />
-import react from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import { playwright } from '@vitest/browser-playwright'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: './playground',
   plugins: [
-    react({
-      babel: { plugins: ['babel-plugin-react-compiler'] },
+    react(),
+    babel({
+      presets: [reactCompilerPreset()],
     }),
   ],
   test: {
